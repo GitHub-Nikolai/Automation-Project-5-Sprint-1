25a63ea98d96390d7766c1f9b704791ec7671490
GitHub-Nikolai <nikolairazer@gmail.com>
2024-06-17 12:03:25 +0300

    Late but made

--- cypress/e2e/myIssue-comments.cy.js
+++ cypress/e2e/myIssue-comments.cy.js
@@ -6,66 +6,46 @@ describe("Issue comments creating, editing and deleting", () => {
       .then((url) => {
         cy.visit(url + "/board");
         cy.contains("This is an issue of type: Task.").click();
-        getIssueDetailsModal, { timeout: 60000 };
       });
   });
 
-  const getIssueDetailsModal = () =>
-    cy.get('[data-testid="modal:issue-details"]');
-
-  it("Add, Edit and Delete issue comment", () => {
+  it("Should create,edit and delete comment successfully", () => {
     const comment = "TEST_COMMENT";
+    const comment_edited = "TEST_COMMENT_EDITED";
 
+    const getIssueDetailsModal = () =>
+      cy.get('[data-testid="modal:issue-details"]');
+    cy.get('[data-testid="modal:issue-details"]'), { timeout: 60000 };
     getIssueDetailsModal().within(() => {
-      getIssueDetailsModal, { timeout: 60000 };
+      //This will Add a comment
       cy.contains("Add a comment...").click();
-
       cy.get('textarea[placeholder="Add a comment..."]').type(comment);
-
       cy.contains("button", "Save").click().should("not.exist");
-
       cy.contains("Add a comment...").should("exist");
       cy.get('[data-testid="issue-comment"]').should("contain", comment);
-    });
-  });
 
-  it("Should edit a comment successfully", () => {
-    const previousComment = "An old silent pond...";
-    const comment = "TEST_COMMENT_EDITED";
-
-    getIssueDetailsModal().within(() => {
+      //This will Edit a comment
       cy.get('[data-testid="issue-comment"]')
         .first()
         .contains("Edit")
         .click()
         .should("not.exist");
-
       cy.get('textarea[placeholder="Add a comment..."]')
-        .should("contain", previousComment)
+        .should("contain", comment)
         .clear()
-        .type(comment);
-
+        .type(comment_edited);
       cy.contains("button", "Save").click().should("not.exist");
-
       cy.get('[data-testid="issue-comment"]')
         .should("contain", "Edit")
-        .and("contain", comment);
+        .and("contain", comment_edited);
+
+      //This will Delete a comment
+      cy.contains("Delete").click();
     });
-  });
-
-  it("Should delete a comment successfully", () => {
-    getIssueDetailsModal()
-      .find('[data-testid="issue-comment"]')
-      .contains("Delete")
-      .click();
-
     cy.get('[data-testid="modal:confirm"]')
       .contains("button", "Delete comment")
       .click()
       .should("not.exist");
-
-    getIssueDetailsModal()
-      .find('[data-testid="issue-comment"]')
-      .should("not.exist");
+    getIssueDetailsModal().contains(comment_edited).should("not.exist");
   });
 });
---
+++ cypress/pages/IssueDetailModal.js
@@ -0,0 +1,218 @@
+class IssueDetailModal {
+  constructor() {
+    this.issueDetailModal = '[data-testid="modal:issue-details"]';
+    this.confirmationModal = '[data-testid="modal:confirm"]';
+    this.timeTrackingModal = '[data-testid="modal:tracking"]';
+    this.getIssueDetailModal = () => cy.get(this.issueDetailModal);
+    this.getLastInput = () => cy.get(this.lastInput);
+    this.getFirstInput = () => cy.get(this.firstInput);
+    this.getTimeTrackingModal = () => cy.get(this.timeTrackingModal);
+    this.checkFirstInputPlaceholderIsNumber = () =>
+      cy.get(this.firstInput).should("have.attr", "placeholder", "Number");
+    this.checkLastInputPlaceholderIsNumber = () =>
+      cy.get(this.lastInput).should("have.attr", "placeholder", "Number");
+    this.addCommentName = "Add a comment...";
+    this.commentTextArea = 'textarea[placeholder="Add a comment..."]';
+    this.clickSaveButton = () =>
+      cy.contains("button", "Save").click().should("not.exist");
+    this.clickCancelButton = () =>
+      cy.contains("button", "Cancel").click().should("not.exist");
+    this.clickDeleteButton = () =>
+      cy.contains("button", "Delete comment").click().should("not.exist");
+    this.clickDoneButton = () =>
+      cy.contains("button", "Done").click().should("not.exist");
+    this.editButtonName = "Edit";
+    this.deleteButtonName = "Delete";
+    this.issueComment = '[data-testid="issue-comment"]';
+    this.estimatedTimeName = "Original Estimate (hours)";
+    this.iconStopwatch = '[data-testid="icon:stopwatch"]';
+    this.iconClose = '[data-testid="icon:close"]';
+    this.hoursEstimated = "h estimated";
+    this.hoursLogged = "h logged";
+    this.hoursRemaining = "h remaining";
+    this.lastInput = "input:last";
+    this.firstInput = "input:first";
+    this.noTimeLoggedText = "No time logged";
+    this.issueTitle = "TEST_TITLE";
+  }
+
+  createComment(comment) {
+    this.getIssueDetailModal().within(() => {
+      cy.contains(this.addCommentName).click();
+      cy.get(this.commentTextArea).type(comment);
+      this.clickSaveButton();
+      cy.contains(this.addCommentName).should("exist");
+    });
+  }
+
+  editComment(previousComment, newComment) {
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.issueComment)
+        .first()
+        .should("contain", previousComment)
+        .contains(this.editButtonName)
+        .click()
+        .should("not.exist");
+
+      cy.get(this.commentTextArea)
+        .should("contain", previousComment)
+        .clear()
+        .type(newComment);
+
+      this.clickSaveButton();
+    });
+  }
+
+  deleteComment(newComment) {
+    this.getIssueDetailModal()
+      .find(this.issueComment)
+      .should("contain", newComment)
+      .contains(this.deleteButtonName)
+      .click();
+
+    cy.get(this.confirmationModal).should("be.visible");
+    this.clickDeleteButton();
+    cy.get(this.confirmationModal).should("not.exist");
+  }
+
+  ensureCommentIsVisible(comment) {
+    cy.get(this.issueComment).should("contain", comment);
+  }
+
+  ensureEditedCommentIsVisible(comment) {
+    cy.get(this.issueComment)
+      .should("contain", this.editButtonName)
+      .and("contain", comment);
+  }
+
+  ensureDeletedCommentIsNotVisible(comment) {
+    this.getIssueDetailModal();
+    cy.get(this.issueComment).contains(comment).should("not.exist");
+  }
+
+  addEstimatedTime(number) {
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.iconStopwatch)
+        .next()
+        .should("contain", this.noTimeLoggedText);
+      this.getLastInput().type(number);
+      cy.get(this.iconStopwatch)
+        .next()
+        .should("contain", `${number}${this.hoursEstimated}`);
+    });
+  }
+
+  editEstimatedTime(previousNumber, newNumber) {
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.iconStopwatch)
+        .next()
+        .should("contain", `${previousNumber}${this.hoursEstimated}`);
+      this.getLastInput().clear().type(newNumber);
+      cy.get(this.iconStopwatch)
+        .next()
+        .should("contain", `${newNumber}${this.hoursEstimated}`);
+    });
+  }
+
+  removeEstimatedTime(previousNumber) {
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.iconStopwatch)
+        .next()
+        .should("contain", `${previousNumber}${this.hoursEstimated}`);
+      this.getLastInput().clear();
+      this.checkLastInputPlaceholderIsNumber();
+      cy.get(this.iconStopwatch)
+        .next()
+        .contains(this.hoursEstimated)
+        .should("not.exist");
+    });
+  }
+
+  ensureEstimatedTimeIsSaved(number) {
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.iconClose).should("be.visible").first().click();
+    });
+    this.getIssueDetailModal().should("not.exist");
+    cy.reload();
+    cy.contains(this.issueTitle).click();
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.iconStopwatch)
+        .next()
+        .should("contain", `${number}${this.hoursEstimated}`);
+    });
+  }
+
+  ensureEstimatedTimeIsEmpty() {
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.iconClose).should("be.visible").first().click();
+    });
+    this.getIssueDetailModal().should("not.exist");
+    cy.reload();
+    cy.contains(this.issueTitle).click();
+    this.getIssueDetailModal().within(() => {
+      this.checkLastInputPlaceholderIsNumber();
+    });
+  }
+
+  addLoggedTime(numberLogged, numberRemaining) {
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.iconStopwatch).click();
+    });
+    this.getTimeTrackingModal()
+      .should("be.visible")
+      .within(() => {
+        cy.get(this.iconStopwatch)
+          .next()
+          .should("contain", this.noTimeLoggedText);
+        this.getFirstInput().type(numberLogged);
+        this.getLastInput().type(numberRemaining);
+        cy.get(this.iconStopwatch)
+          .next()
+          .should("contain", `${numberLogged}${this.hoursLogged}`)
+          .and("contain", `${numberRemaining}${this.hoursRemaining}`);
+        this.clickDoneButton();
+        this.getTimeTrackingModal().should("not.exist");
+      });
+  }
+
+  removeLoggedTime() {
+    this.getIssueDetailModal().within(() => {
+      cy.get(this.iconStopwatch).click();
+    });
+    this.getTimeTrackingModal()
+      .should("be.visible")
+      .within(() => {
+        this.getFirstInput().clear();
+        this.getLastInput().clear();
+        this.checkFirstInputPlaceholderIsNumber();
+        this.checkLastInputPlaceholderIsNumber();
+        cy.get(this.iconStopwatch)
+          .next()
+          .should("contain", this.noTimeLoggedText);
+        this.clickDoneButton();
+        this.getTimeTrackingModal().should("not.exist");
+      });
+  }
+
+  ensureTimeWasLogged(numberLogged, numberRemaining) {
+    this.getIssueDetailModal()
+      .should("be.visible")
+      .within(() => {
+        cy.contains(this.noTimeLoggedText).should("not.exist");
+        cy.get(this.iconStopwatch)
+          .next()
+          .should("contain", `${numberLogged}${this.hoursLogged}`)
+          .and("contain", `${numberRemaining}${this.hoursRemaining}`);
+      });
+  }
+
+  ensureNoTimeIsLogged() {
+    this.getIssueDetailModal()
+      .should("be.visible")
+      .within(() => {
+        cy.contains(this.noTimeLoggedText).should("be.visible");
+      });
+  }
+}
+
+export default new IssueDetailModal();
